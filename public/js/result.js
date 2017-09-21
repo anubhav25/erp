02ef4402e1b22685fa4f24c5e969a9bac02da10b
anubhav25 <gupta.anubhav25@gmail.com>
2017-09-21 16:56:43 +0530

    updated

--- public/js/result.js
+++ public/js/result.js
@@ -6,5 +6,6 @@
 $("#submit").click(function (){
     var sem=window.btoa($("#sem").val());
     var rno= window.btoa("2514221").val();
+    console.log(sem+rno);
 window.location.href = "http://49.50.77.75/Forms/Student/PrintReportCard.aspx?rollno="+rno+"&sem="+sem;
 });
