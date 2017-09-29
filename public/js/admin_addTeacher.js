9f4995550430ab1270c4370903b331a2664f5411
anubhav25 <gupta.anubhav25@gmail.com>
2017-09-29 14:36:02 +0530

    admin_addTeacher.js

--- public/js/admin_addTeacher.js
+++ public/js/admin_addTeacher.js
@@ -77,11 +77,28 @@ var country=$('#country').val();
 var tcDetails=new teacherDetails(name,mob_no,dob,religion,bg,gender,Nationality,email,voter_card_no,aadhar_card_no,address,city,distt,state,pin,landline,country);
 
 console.log(tcDetails);
-	$.post('/addTeacher',tcDetails,function (data) {
+	$.post('/setTeacherDetails',tcDetails,function (data) {
 			console.log(data)
-			if(data.msg=="ok")
+			if(data.msg==="ok")
 			{
-				
+				alert("Teacher Added");
+                $('#name').val('');
+                $('#mob_no').val('');
+                $('#dob').val('');
+                $('#religion').val('');
+                $('#bg').val('');
+                $('#gender').val('');
+                $('#Nationality').val('');
+                $('#email').val('');
+                $('#voter_card_no').val('');
+                $('#aadhar_card_no').val('');
+                $('#address').val('');
+                $('#city').val('');
+                $('#distt').val('');
+                $('#state').val('');
+                $('#pin').val('');
+                $('#landline').val('');
+                $('#country').val('');
 			}
 			else
 			{
@@ -91,35 +108,10 @@ console.log(tcDetails);
 
 
 
-//});
-/*
+});
+
 $('form').submit(function(){
-
-
-	var name=$('#name').val();
-var mob_no=$('#mob_no').val();
-var dob=$('#dob').val();
-var religion=$('#religion').val();
-var bg=$('#bg').val();
-var gender=$('#gender').val();
-var Nationality=$('#Nationality').val();
-var email=$('#email').val();
-var voter_card_no=$('#voter_card_no').val();
-var aadhar_card_no=$('#aadhar_card_no').val();
-var address=$('#address').val();
-var city=$('#city').val();
-var distt=$('#distt').val();
-var state=$('#state').val();
-var pin=$('#pin').val();
-var landline=$('#landline').val();
-var country=$('#country').val();
-
-var tcDetails=new teacherDetails(name,mob_no,dob,religion,bg,gender,Nationality,email,voter_card_no,aadhar_card_no,address,city,distt,state,pin,landline,country);
-console.log(tcDetails);
-
-
-	return false;});*/
-
+	return false;});
 
 
 
